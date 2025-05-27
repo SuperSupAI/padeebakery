ce60524f689161a0e33cf9d8d298551b92b9ad0e
June <peedapus@gmail.com>
2025-05-27 16:30:20 +0700

    add storages

--- padeebakery/settings.py
+++ padeebakery/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
