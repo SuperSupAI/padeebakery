3d2aeaf24c89c3cf32c755e5c6d1a65477cb0859
June <peedapus@gmail.com>
2025-05-27 13:38:55 +0700

    requirements

--- padeebakery/wsgi.py
+++ padeebakery/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys,site
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'padeebakery.settings')
+sys.path.append('D:/Python/projects/padeebakery')
+sys.path.append('D:/Python/projects/padeebakery/padeebakery')
+os.environ["DJANGO_SETTINGS_MODULE"] =  'padeebakery.settings'
 
 application = get_wsgi_application()
