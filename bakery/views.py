ffde8acb9ea294128f02c342a35841ab379864f3
June <peedapus@gmail.com>
2025-12-11 09:58:01 +0700

    change menu in views

--- bakery/views.py
+++ bakery/views.py
@@ -8,46 +8,48 @@ import json
 BREAD_DATA = {
     "base": [
         { 
-            "id": 'sourdough', 
-            "name": 'Sourdough', 
+            "id": 'ourdough_bread', 
+            "name": 'Sourdough Bread', 
             "price": 120, 
             "icon": 'fa-bread-slice', 
             "desc": 'แป้งหมักธรรมชาติ เปลือกกรอบ เนื้อเหนียวนุ่ม',
             # ต้องแก้พาธรูปภาพให้ชี้ไปที่ Static Files ของ Django
-            "image": '/static/bakery/img/base/sourdough.png' # <--- แก้ไข
-        },
-        { 
-            "id": 'wholewheat', 
-            "name": 'Whole Wheat 100%', 
-            "price": 140, 
-            "icon": 'fa-wheat-awn', 
-            "desc": 'ไฟเบอร์สูง ธัญพืชเต็มเมล็ด หอมมัน',
-            "image": '/static/bakery/img/base/wholewheat.png' # <--- แก้ไข
+            "image": 'static/bakery/img/base/sourdough.jpg' # <--- แก้ไข
         },
         { 
             "id": 'shokupan', 
-            "name": 'Shokupan Milk', 
+            "name": 'Shokupan (Loaf)', 
             "price": 150, 
             "icon": 'fa-cube', 
             "desc": 'ขนมปังนมสไตล์ญี่ปุ่น นุ่มละลายในปาก',
-            "image": '/static/bakery/img/base/shokupan.png' # <--- แก้ไข
+            "image": 'static/bakery/img/base/shokupan.png' # <--- แก้ไข
         },
     ],
 
     "mixins": [
-        { "id": 'cranberry', "name": 'แครนเบอร์รี่', "price": 30, "icon": 'fa-circle', "color": 'text-red-500', "image": '/static/bakery/img/icon/cranberry.png' }, # เพิ่ม image/icon ที่ถูกต้อง
-        { "id": 'walnut', "name": 'วอลนัท', "price": 40, "icon": 'fa-brain', "color": 'text-amber-700', "image": '/static/bakery/img/icon/walnut.png' },
-        { "id": 'cheese', "name": 'เชดด้าชีส', "price": 35, "icon": 'fa-cheese', "color": 'text-yellow-400', "image": '/static/bakery/img/icon/cheese.png' },
-        { "id": 'chocolate', "name": 'ดาร์กช็อกโกแลต', "price": 35, "icon": 'fa-cookie', "color": 'text-gray-700', "image": '/static/bakery/img/icon/chocolate.png' },
-        { "id": 'raisin', "name": 'ลูกเกดทอง', "price": 25, "icon": 'fa-cookie-bite', "color": 'text-yellow-600', "image": '/static/bakery/img/icon/raisin.png' },
-        { "id": 'pumpkin', "name": 'เมล็ดฟักทอง', "price": 30, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/pumpkin.png' },
+        { "id": 'none', "name": 'ไม่ใส่', "price": 0, "icon": 'fa-circle', "color": 'text-red-500', "image": '/static/bakery/img/icon/none.png' },
+        { "id": 'wholewheat', "name": 'Whole Wheat', "price": 15, "icon": 'fa-circle', "color": 'text-red-500', "image": '/static/bakery/img/icon/wholewheat.png' },
+    #    { "id": 'cranberry', "name": 'แครนเบอร์รี่', "price": 30, "icon": 'fa-circle', "color": 'text-red-500', "image": '/static/bakery/img/icon/cranberry.png' }, # เพิ่ม image/icon ที่ถูกต้อง
+    #    { "id": 'walnut', "name": 'วอลนัท', "price": 40, "icon": 'fa-brain', "color": 'text-amber-700', "image": '/static/bakery/img/icon/walnut.png' },
+    #    { "id": 'cheese', "name": 'เชดด้าชีส', "price": 35, "icon": 'fa-cheese', "color": 'text-yellow-400', "image": '/static/bakery/img/icon/cheese.png' },
+    #    { "id": 'chocolate', "name": 'ดาร์กช็อกโกแลต', "price": 35, "icon": 'fa-cookie', "color": 'text-gray-700', "image": '/static/bakery/img/icon/chocolate.png' },
+        { "id": 'raisin', "name": 'ลูกเกด', "price": 25, "icon": 'fa-cookie-bite', "color": 'text-yellow-600', "image": '/static/bakery/img/icon/raisin.png' },
+        { "id": 'pumpkin', "name": 'เมล็ดฟักทอง', "price": 25, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/pumpkin.png' },
+        { "id": 'sunflower', "name": 'เมล็ดทานตะวัน', "price": 25, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/sunflower.png' },
+        { "id": 'flaxseed', "name": 'เมล็ดแฟลกซ์', "price": 25, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/flaxseed.png' },
+        { "id": 'sesame', "name": 'งาดำ/ขาว', "price": 25, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/sesame.png' },
+        { "id": 'quinoa', "name": 'ควินัว', "price": 25, "icon": 'fa-seedling', "color": 'text-green-600', "image": '/static/bakery/img/icon/quinoa.png' },
     ],
 
     "toppings": [
         { "id": 'none', "name": 'ไม่ใส่', "price": 0, "icon": 'fa-ban' },
+        { "id": 'pumpkin', "name": 'เมล็ดฟักทอง', "price": 10, "icon": 'fa-ellipsis', "color": 'text-gray-800' },
+        { "id": 'sunflower', "name": 'เมล็ดทานตะวัน', "price": 10, "icon": 'fa-ellipsis', "color": 'text-gray-800' },
+    #    { "id": 'flaxseed', "name": 'เมล็ดแฟลกซ์', "price": 10, "icon": 'fa-ellipsis', "color": 'text-gray-800' },
         { "id": 'sesame', "name": 'งาขาว/ดำ', "price": 10, "icon": 'fa-ellipsis', "color": 'text-gray-800' },
-        { "id": 'oats', "name": 'ข้าวโอ๊ต', "price": 15, "icon": 'fa-leaf', "color": 'text-amber-200' },
-        { "id": 'icing', "name": 'ไอซิ่ง', "price": 10, "icon": 'fa-snowflake', "color": 'text-blue-200' },
+    #    { "id": 'quinoa', "name": 'ควินัว', "price": 10, "icon": 'fa-ellipsis', "color": 'text-gray-800' },
+    #    { "id": 'oats', "name": 'ข้าวโอ๊ต', "price": 15, "icon": 'fa-leaf', "color": 'text-amber-200' },
+    #    { "id": 'icing', "name": 'ไอซิ่ง', "price": 10, "icon": 'fa-snowflake', "color": 'text-blue-200' },
     ]
 }
 
